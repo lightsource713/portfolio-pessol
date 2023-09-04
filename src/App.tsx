2be0724c761a4c49433aacac09b9248fc31e6407
lightSource0713 <kingstone0713@gmail.com>
2023-09-03 21:09:05 -0300

    reupdate em react vite

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './assets/Fonts/fonts.css';
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <div className="container">
+    <p>Ana Beatriz Martins</p>
+    <img src="./assets/img/boneco-notebook.png" alt="" />
+      
+    </div>
+    </>
+  )
+}
+
+export default App
