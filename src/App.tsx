750b592e70efc9f0ca288dca666496c7e6ed6ad8
lightSource0713 <kingstone0713@gmail.com>
2023-09-08 23:03:17 -0300

    home

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import './assets/Fonts/fonts.css';
-import './App.css'
+import "./assets/Fonts/fonts.css";
+import "./App.css";
 
 function App() {
-  
   return (
     <>
-    <div className="container">
-    <p>Ana Beatriz</p>
-    <img src="../public/pbc.png" alt="" />
-    </div>
+      <div className="container">
+        <div className="text">
+          <p id="title">Ana Beatriz</p>
+          <p id="subtitle">Bem vindo ao meu humilde portfólio!</p>
+          <button id="start">
+            <p>Start</p>
+            <img src="./icon-start.svg" />
+          </button>
+        </div>
+        <div className="img">
+        <img className="img3d" src="./pbc.png" />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
