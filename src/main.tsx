c9c93aed902d573d0335d1249dce9401daefe3a7
lightSource0713 <kingstone0713@gmail.com>
2023-10-17 09:17:58 -0300

    imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import Home from './screens/Home'
-import About from './screens/About'
-import Contact from './screens/Contact'
+import Home from './screens/Home/index'
+import About from './screens/About/index'
+import Contact from './screens/Contact/index'
 
 import './index.css'
 
