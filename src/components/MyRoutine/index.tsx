7845aa83083bdea841f82bed9ba57d758da6ac00
lightSource0713 <kingstone0713@gmail.com>
2023-10-17 00:29:16 -0300

    adding MyRoutine component

---
+++ src/components/MyRoutine/index.tsx
@@ -0,0 +1,70 @@
+import './MyRoutine.css'
+
+
+export default function MyRoutine() {
+    return (
+        <>
+            <div className="section routine">
+                <img src="./boneco-task.png" />
+                <table className="table">
+                    <tr>
+                        <th>Horário</th>
+                        <th>Segunda</th>
+                        <th>Terça</th>
+                        <th>Quarta</th>
+                        <th>Quinta</th>
+                        <th>Sexta</th>
+                    </tr>
+                    <tr>
+                        <td>7:40</td>
+                        <td>Desenvolvimento de Sistemas</td>
+                        <td>Ciências Humanas</td>
+                        <td>Linguagens</td>
+                        <td>Banco de Dados</td>
+                        <td>Desenvolvimento de Sistemas</td>
+                    </tr>
+                    <tr>
+                        <td>10:10</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                    </tr>
+                    <tr>
+                        <td>10:30</td>
+                        <td>Modelagem de Sistemas</td>
+                        <td>Ciências da Natureza</td>
+                        <td>Programação de Aplicativos</td>
+                        <td>Desenvolvimento de Sistemas</td>
+                        <td>Matemática</td>
+                    </tr>
+                    <tr>
+                        <td>13:00</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                    </tr>
+                    <tr>
+                        <td>15:00</td>
+                        <td>Academia</td>
+                        <td>Academia</td>
+                        <td>Academia</td>
+                        <td>Academia</td>
+                        <td>Academia</td>
+                    </tr>
+                    <tr>
+                        <td>18:00</td>
+                        <td>Projetos / Estudo</td>
+                        <td>Projetos / Estudo</td>
+                        <td>Projetos / Estudo</td>
+                        <td>Projetos / Estudo</td>
+                        <td>Projetos / Estudo</td>
+                    </tr>
+                </table>
+            </div>
+        </>
+    )
+}
--- src/screens/About/index.tsx
+++ src/screens/About/index.tsx
@@ -2,22 +2,27 @@ import './About.css'
 import Header from '../../components/Header'
 import AboutMe from '../../components/AboutMe';
 import MySkills from '../../components/MySkills';
+import MyRoutine from '../../components/MyRoutine';
 
 function About() {
     return (
         <>
             <Header />
             <div className="container">
-                <div className="tag">
+                {/* <div className="tag">
                     <p>About</p>
-                </div>
+                </div> */}
                 <p className='title'>{"<AboutMe />"}</p>
             </div>
             <AboutMe />
-            <div className="container">
+            <div className="container containerSkills">
             <p className='title'>{"<MySkills />"}</p>
             </div>
             <MySkills />
+            <div className="container containerRoutine">
+            <p className='title'>{"<MyRoutine />"}</p>
+            </div>
+            <MyRoutine />
         </>
     )
 }
