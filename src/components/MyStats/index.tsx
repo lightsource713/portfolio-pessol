1843ef6f67a0e092469e11efeceb18ccf5f3f024
lightSource0713 <kingstone0713@gmail.com>
2023-11-09 22:36:50 -0300

    :art: update structure

--- src/components/MyStats/index.tsx
+++ src/components/MyStats/index.tsx
@@ -14,6 +14,10 @@ export default function MyStats() {
 
     const [nomeUsuario] = useState<string>("anabmartins");
     const [userData, setUserData] = useState<UserData | null>(null);
+    
+    const statsUrl = `https://github-profile-summary-cards.vercel.app/api/cards/stats?username=${nomeUsuario}&theme=default`;
+
+    const mostCommitLanguageUrl = `https://github-profile-summary-cards.vercel.app/api/cards/most-commit-language?username=${nomeUsuario}&theme=default&exclude=HTML,CSS,Portugol`;
 
     useEffect(() => {
         if (nomeUsuario) {
@@ -34,10 +38,10 @@ export default function MyStats() {
                 <img src="./bncpoint.png" className="statsImg" />
                 {userData && (
                     <>
-                    <a href="https://github.com/anabmartins" target='_blank'>
-                        <img src={userData.avatar_url} className='gitImg'/>
-                    </a>
-                    <p className='title'>GitHub Stats</p>
+                        <a href="https://github.com/anabmartins" target='_blank'>
+                            <img src={userData.avatar_url} className='gitImg' />
+                        </a>
+                        <p className='title'>GitHub Stats</p>
                         <div className="userData">
                             <p><strong>Nome:</strong> {userData.name}</p>
                             <p><strong>Nickname:</strong> {userData.login}</p>
@@ -45,12 +49,12 @@ export default function MyStats() {
                             <p> <strong>Seguidores:</strong> {userData.followers}</p>
                             <p> <strong>Seguindo:</strong> {userData.following}</p>
                         </div>
+                        <div className="gitstats">
+                            <img src={statsUrl} alt="" />
+                            <img src={mostCommitLanguageUrl} />
+                        </div>
                     </>
                 )}
-                <div className="gitstats">
-                <img src="https://github-profile-summary-cards.vercel.app/api/cards/stats?username=anabmartins&theme=default" alt="" />
-                <img src="http://github-profile-summary-cards.vercel.app/api/cards/most-commit-language?username=anabmartins&theme=default&exclude=HTML,CSS,Portugol"/>
-                </div>
                 {/* <p className='title'>TryHackMe</p>
                 <a href="https://tryhackme.com/p/BeatrMart" target='_blank'>
                 <img src="https://tryhackme-badges.s3.amazonaws.com/BeatrMart.png" alt="TryHackMe"/>
